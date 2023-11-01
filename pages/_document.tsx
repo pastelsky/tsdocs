ea9500900c6092b96f76fa4aca7577387438b5d0
Shubham Kanodia <shubham.kanodia10@gmail.com>
2023-11-02 02:28:44 +0530

    Added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,29 @@ export default function Document() {
           content="Find type documentation for any npm library that ships with types or has publically available types on Definitely Typed"
         />
 
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2c75d5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#2C75D5" />
+
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://tsdocs.dev/" />
         <meta
