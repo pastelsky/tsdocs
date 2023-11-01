5863e78310c200b6b719f065df896690d1395cdc
Shubham Kanodia <shubham.kanodia10@gmail.com>
2023-11-02 02:10:12 +0530

    Add og tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,42 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200;400;600&display=swap"
           rel="stylesheet"
         />
+        <title>TS Docs | Reference docs for npm packages</title>
+        <meta
+          name="title"
+          content="TS Docs | Reference docs for npm packages"
+        />
+        <meta
+          name="description"
+          content="Find type documentation for any npm library that ships with types or has publically available types on Definitely Typed"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://tsdocs.dev/" />
+        <meta
+          property="og:title"
+          content="TS Docs | Reference docs for npm packages"
+        />
+        <meta
+          property="og:description"
+          content="Find type documentation for any npm library that ships with types or has publically available types on Definitely Typed"
+        />
+        <meta property="og:image" content="https://tsdocs.dev/og-image.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://tsdocs.dev/" />
+        <meta
+          property="twitter:title"
+          content="TS Docs | Reference docs for npm packages"
+        />
+        <meta
+          property="twitter:description"
+          content="Find type documentation for any npm library that ships with types or has publically available types on Definitely Typed"
+        />
+        <meta
+          property="twitter:image"
+          content="https://tsdocs.dev/og-image.png"
+        />
       </Head>
       <body>
         <Main />
