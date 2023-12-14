fe4acd5389d21d9521eaff3e6541e092d9ff942d
Shubham Kanodia <shubham.kanodia10@gmail.com>
2023-12-14 08:33:44 +0530

    Increase timeout to 100 seconds

--- client/api/get-package-docs.ts
+++ client/api/get-package-docs.ts
@@ -26,7 +26,7 @@ type PollAPIResponse =
     };
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-const POLL_TIMEOUT = 80000;
+const POLL_TIMEOUT = 100_000;
 
 type PackageDocsResponse =
   | {
