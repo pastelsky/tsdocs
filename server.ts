90fc7a34679b0aaae5ec7dab97ef8dbd081d8d35
Shubham Kanodia <shubham.kanodia10@gmail.com>
2023-12-14 08:12:03 +0530

    Only warn logs

--- server.ts
+++ server.ts
@@ -25,13 +25,7 @@ const port = 3000;
 
 const fastify = fastifyStart({
   logger: {
-    transport: {
-      target: "pino-pretty",
-      options: {
-        translateTime: "HH:MM:ss Z",
-        ignore: "pid,hostname",
-      },
-    },
+    level: "warn",
   },
 });
 
