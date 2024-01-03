81b83ef7f64d51b8e1d442788e0e9c70525baf41
Shubham Kanodia <shubham.kanodia10@gmail.com>
2024-01-03 18:57:59 +0530

    Add timeout to workers

--- server/workers/docs-builder-worker.js
+++ server/workers/docs-builder-worker.js
@@ -22,17 +22,39 @@ setInterval(() => {
   }
 }, 5000);
 
+function promiseTimeout(promise, timeout) {
+  const timeoutPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(
+        new Error(
+          `PROMISE_TIMEOUT: Promise timed out after ${timeout} seconds`,
+        ),
+      );
+    }, timeout);
+  });
+
+  return Promise.race([timeoutPromise, timeout]);
+}
+
 module.exports = async (job) => {
   try {
     workerActiveTime = Date.now();
-    return await generateDocsForPackage(job.data.packageJSON, {
-      force: job.data.force,
-    });
+    const results = await promiseTimeout(
+      generateDocsForPackage(job.data.packageJSON, {
+        force: job.data.force,
+      }),
+      120,
+    );
+    return results;
   } catch (err) {
+    const isWorkerTimeout = err.message.includes("PROMISE_TIMEOUT");
+
+    const errorCode = isWorkerTimeout ? "DOCS_BUILD_TIMEOUT" : err.message;
+
     job.updateData({
       ...job.data,
       originalError: {
-        code: err.message,
+        code: errorCode,
         stacktrace: err?.originalError?.stack,
         message: err?.originalError?.message,
       },
